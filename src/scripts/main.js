f985e28f30648828e400b0793201cd6c641f29d3
Elige Liashenko <funcan.100@gmail.com>
2025-04-23 08:07:43 +0300

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,28 @@
+const btns = document.querySelectorAll('[data-category]');
+const productsContainer = document.querySelector('.products-container');
+
+btns.forEach(btn => btn.addEventListener('click', function (e) {
+    const category = btn.dataset.category;
+
+    fetch(`./data/db-${category}.json`)
+        .then(res => res.json())
+        .then(data => renderProducts(data))
+}))
+
+function renderProducts(products) {
+    productsContainer.innerHTML = "";
+    products.forEach(p => {
+        productsContainer.innerHTML += `
+         <div class="product-card">
+            <img src="./src/img/${p.urlImg}" alt="${p.name}">
+            <h3>${p.name}</h3>
+            <p>${p.price}</p>
+            <button>Купити</button>
+        </div>
+        `
+    })
+}
+
+
+
+
